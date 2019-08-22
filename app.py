a4cbec42bc996dbc4efe42a2d77178a0c60032b6
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-08-22 14:05:52 +0700

    akses form metode post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,14 @@ def index():
 @app.route('/profile/<username>')
 def show_profile(username):
     return render_template('profile.html', username=username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def show_login():
+	if request.method == 'POST':
+		return 'Email kamu adalah ' + request.form['email']
+
+	return render_template('login.html')
+
 
 if __name__ == '__main__':
    app.run(debug=True)
